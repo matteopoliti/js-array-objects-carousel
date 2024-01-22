3c2bc84f8e21b5bbb591b4cc5f8ff4928a8cb3d6
matteopoliti <matteo.politi17@gmail.com>
2024-01-22 15:58:18 +0100

    feat: add html structure

---
+++ assets/javascript/main.js
@@ -0,0 +1,31 @@
+const animeArray = [
+    {
+      urlImmagine: "./assets/img/one-piece.webp",
+      titolo: "One Piece",
+      descrizione: "La storia di Monkey D. Luffy e della sua ciurma che cercano il leggendario tesoro One Piece."
+    },
+    {
+      urlImmagine: "./assets/img/aot-fury-titan-desktop-wallpaper-preview.jpg",
+      titolo: "Attack on Titan",
+      descrizione: "Un mondo in cui l'umanità lotta per sopravvivere contro enormi creature chiamate Titani."
+    },
+    {
+        urlImmagine: "./assets/img/JJK-header-mobile2.webp",
+        titolo: "Jujutsu Kaisen",
+        descrizione: "Yuuji Itadori entra a far parte di una scuola di magia oscura per combattere i maledetti e proteggere il mondo."
+    },
+    {
+      urlImmagine: "./assets/img/demon-slayer-4k-pictures-5v5lz47uso2tx2kr.jpg",
+      titolo: "Demon Slayer",
+      descrizione: "Tanjiro Kamado cerca vendetta contro i demoni che hanno ucciso la sua famiglia e trasformato sua sorella in un demone."
+    },
+    {
+        urlImmagine: "./assets/img/spider-man-across-6016x3384-11595.jpg",
+        titolo: "Spider-Man: Across the Spider-Verse",
+        descrizione: "Il seguito di Spider-Man: Into the Spider-Verse, in cui Miles Morales esplora nuovi universi paralleli."
+    },
+];
+  
+const arrowLeft = document.querySelector('.fa-circle-chevron-left')
+
+const arrowRight = document.querySelector('.fa-circle-chevron-right')
